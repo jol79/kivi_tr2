1abcd9737bdf28b52d6cacff6ce3d233cfd64e20
jol79 <jolhuck@gmail.com>
2020-09-20 22:54:34 +0300

    function for button to get values from textinput widget, and store this data in variables

--- app_design.py
+++ app_design.py
@@ -1,18 +1,38 @@
 import kivy as kivy
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
 
 
 # initializing gridlayout widget with label:
 class MainPage(Widget):
-    # def __init__(self, **kwargs):
-    #     super(MainPage, self).__init__(**kwargs)
-    #     self.add_widget(Label(text="Hello on this page"))
-    pass
+
+    # initializing variables to get from kv file:
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+    keyword = ObjectProperty(None)
+
+    def btn(self):
+        print("email: ", self.email.text,
+              ", password: ", self.password.text,
+              ", keyword: ", self.keyword.text)
+
+
+# # button widget:
+# class Launch(BoxLayout):
+#     def __init__(self, **kwargs):
+#         super(Launch, self).__init__(**kwargs)
+#         search_button = Button(
+#                 text="Search",
+#                 size=(80, 80),
+#                 size_hint=(None, None)
+#         )
+#         # search_button.bind(on_press=self.)
 
 
 ###
